8c59f0e4cd41bf16b8747075e59f5abf4ad3892b
johny8999 <sinaalipour77@hotmail.com>
2024-06-26 12:24:52 +0330

    migration

---
+++ Infra.Data/Migrations/20240626084008_mg2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "TblSubEntity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "TblSubEntity",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
